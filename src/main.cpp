51dc1a8f271d058cd4d6ba291e0bb6594d268d09
Dreyes-hash <126710580+Dreyes-hash@users.noreply.github.com>
2023-06-22 02:07:43 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,21 @@
 #include <Adafruit_SSD1306.h>
 #include <IRremote.h>
 #include "SSD1306_Defs.h"
+
 void setLED();
 void sendSignal();
+
 float voltage;
 int IRPin = PA4; 
 int ledMode = 0;
-IRsend irsend(IRPin);
+IRsend irsend(IRPin); //configura el pin PA4 para enviar la señal
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 void setup() {
-  pinMode(PA5, INPUT);
+  // configura el puerto PA5,PA7 Como entradas
+  pinMode(PA5, INPUT);  
   pinMode(PA7, INPUT);
-  attachInterrupt(PA5, sendSignal, RISING);
+  attachInterrupt(PA5, sendSignal, RISING); // configura la interrupción de PA5
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Inicializar la pantalla OLED
   display.setTextColor(SSD1306_WHITE);
 }
@@ -22,7 +25,7 @@ void setup() {
 void loop() {
   if (digitalRead(PA7) == HIGH) {
     	setLED(); // Realizar el cambio de estado del LED si el botón PA7 está presionado
-    	delay(50); // Agregar un retardo para evitar lecturas múltiples del botón debido al rebote
+    	delay(50); // Agregar un retardo 
   }
   display.clearDisplay();
   display.setTextSize(2);
@@ -40,7 +43,7 @@ void loop() {
 void setLED() {
   if (ledMode==0){
         display.clearDisplay();
-  	voltage=3.3;
+  	voltage=330;
   	ledMode=1;
   	display.setCursor(0, 0);
   	display.print("Led ON");
@@ -53,8 +56,8 @@ void setLED() {
   	ledMode=0;
   }
   // Convertir el valor de percentage a un formato adecuado para el protocolo 
-  unsigned long tData = map(voltage, 0, 3.3, 0, 0xFFFF); // Convertir el porcentaje a un valor adecuado para el protocolo de Sony
-  irsend.sendSony(0x1,tData, 4); // Enviar la señal de infrarrojos con los datos convertidos
+  unsigned long tData = map(voltage, 0, 330, 0, 0xFFFF); // Convertir el porcentaje a un valor adecuado para el protocolo NEC
+  irsend.sendNEC(0x0,tData, 4); // Enviar la señal de infrarrojos con los datos convertidos
   
   display.display();
   delay(2000);	
@@ -62,13 +65,13 @@ void setLED() {
 
 void sendSignal() {
   // Convertir el valor de percentage a un formato adecuado para el protocolo 
-  unsigned long tData = voltage//map(voltage, 0, 3.3, 0, 0xFFFF); // Convertir el porcentaje a un valor adecuado para el protocolo de Sony
-  irsend.sendSony(0x1,tData, 4); // Enviar la señal de infrarrojos con los datos convertidos
+  voltage=voltage*100;
+  unsigned long tData = map(voltage, 0, 330, 0, 0xFFFF); // Convertir el porcentaje a un valor adecuado para el protocolo NEC
+  irsend.sendNEC(0x0,tData, 4); // Enviar la señal de infrarrojos con los datos convertidos
   display.setCursor(0, 0);
   display.print("enviado");
   display.display();
   delay(300);	
 }
-
 
 
